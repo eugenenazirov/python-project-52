3b7936d8a8d212309f9a60bec7b6c146748d3cec
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-09-05 16:28:16 +0600

    Update urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,7 +15,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task_manager import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="base.html"), name='home'),
+    path('users/', views.UserList.as_view(), name='users_list'),
+    path('users/<int:pk>/details/', views.UsersDetail.as_view(), name='users_details'),
+    path('users/create/', views.UserCreate.as_view(), name='users_create'),
+    path('users/<int:pk>/update/', views.UserUpdate.as_view(), name='users_update'),
+    path('users/<int:pk>/delete/', views.UserDelete.as_view(), name='users_delete'),
+    path('login/', views.LoginUser.as_view(), name='user_login'),
+    path('logout/', views.logout_user, name='user_logout'),
     path('admin/', admin.site.urls),
+    path('test/', views.TestPage.as_view(), name="test_page"),
+    path('test2/', views.TestPage2.as_view(), name="test_page2"),
+    path('statuses/', views.StatusList.as_view(), name='statuses_list'),
+    path('statuses/create/', views.StatusCreate.as_view(), name='statuses_create'),
+    path('statuses/<int:pk>/update/', views.StatusUpdate.as_view(), name='statuses_update'),
+    path('statuses/<int:pk>/delete/', views.StatusDelete.as_view(), name='statuses_delete'),
+    path('tasks/', views.TaskListWithFilter.as_view(), name='tasks_list'),
+    path('tasks/create/', views.TaskCreate.as_view(), name='tasks_create'),
+    path('tasks/<int:pk>/update/', views.TaskUpdate.as_view(), name='tasks_update'),
+    path('tasks/<int:pk>/delete/', views.TaskDelete.as_view(), name='tasks_delete'),
+    path('tasks/<int:pk>/', views.TaskDetail.as_view(), name='tasks_detail'),
+    path('labels/', views.LabelList.as_view(), name='labels_list'),
+    path('labels/create/', views.LabelCreate.as_view(), name='labels_create'),
+    path('labels/<int:pk>/update/', views.LabelUpdate.as_view(), name='labels_update'),
+    path('labels/<int:pk>/delete/', views.LabelDelete.as_view(), name='labels_delete'),
 ]
